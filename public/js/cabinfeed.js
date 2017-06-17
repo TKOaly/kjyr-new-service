c0fc1eb9c3baf62fdc6cd0032aa7859ce68d4018
MergHQ <hugo.holm@live.com>
2017-06-17 19:22:38 +0300

    Autmatically select websocket protocol

--- public/js/cabinfeed.js
+++ public/js/cabinfeed.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket('ws://' + window.location.hostname + '/cabins');
+const socket = new WebSocket(`${location.protocol === 'https:' ? 'wss' : 'ws'}` + window.location.hostname + '/cabins');
 
 socket.onopen = function () {
   console.log('Socket opened');
