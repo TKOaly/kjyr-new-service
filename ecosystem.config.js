d60662ca29550838af1f6742bf6d66d61ce3be7d
Hugo Holmqvist <hugo.holm@live.com>
2017-06-27 16:34:34 +0300

    Fix ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -28,7 +28,7 @@ module.exports = {
       host : 'haba.tko-aly.fi',
       ref  : 'origin/master',
       "pre-deploy-local" : "whoami",
-      repo : 'git@gitlab.com:tko-aly/mayhem.git',
+      repo : 'git@github.com:TKOaly/mayhem.git',
       path : '/home/mayhem/mayhem',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     }
