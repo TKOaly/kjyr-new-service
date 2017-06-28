39440ef2a08e79e65f1139084869cd9ec853d5b4
MergHQ <hugo.holm@live.com>
2017-06-28 14:54:42 +0300

    Comment some stuff

--- src/controllers/EventFeeder.js
+++ src/controllers/EventFeeder.js
@@ -1,8 +1,15 @@
 const WebSocket = require('ws');
 
+/**
+ * Dope class for sending real-time events via websocket connection
+ */
 class EventFeeder {
   constructor() {}
 
+  /**
+   * Creates the websocket connection
+   * @param {String} path ws path 
+   */
   init(path) {
     this.wss = new WebSocket.Server({
       port: 4200,
@@ -10,6 +17,10 @@ class EventFeeder {
     });
   }
 
+  /**
+   * Sends an event
+   * @param {Object} data Will be stringyfied to JSON
+   */
   send(data) {
     this.wss.clients.forEach(client => {
       if (client.readyState === WebSocket.OPEN) {
