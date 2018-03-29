85eef83a0977fc03abfeb89401650d3c7d9cfd63
MergHQ <hugo.holm@live.com>
2018-03-29 19:30:33 +0300

    Add nullchecks for preferences

--- src/models/Preference.ts
+++ src/models/Preference.ts
@@ -1,19 +1,24 @@
-import { Table, Column, Model, BelongsTo, ForeignKey, HasOne, HasMany, BelongsToMany } from 'sequelize-typescript';
+import { Table, Column, Model, BelongsTo, ForeignKey, HasOne, HasMany, BelongsToMany, AllowNull, Length } from 'sequelize-typescript';
 import PersonPreferences from './PersonPreference';
 import Person from './Person';
 
 @Table({ timestamps: true })
 export default class Preference extends Model<Preference> {
 
+  @AllowNull(false)
+  @Length({ min: 1 })
   @Column
   name: string;
 
+  @AllowNull(false)
+  @Length({ min: 1 })
   @Column
   description: string;
 
+  @AllowNull(false)
   @Column
   price: number;
 
   @BelongsToMany(() => Person, () => PersonPreferences)
-  preferences: Preference[];
+  persons: Person[];
 }
